4c6ae3a9fe234f1b3573cea942019082518ffff5
sp3ncerlan <lansp329@gmail.com>
2026-03-02 15:57:49 -0600

    Limiting snippetCreate to a POST method

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -21,6 +21,12 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
