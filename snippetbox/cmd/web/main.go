8d2075e7db17ec2ff377e03adfc35ad15fc53ee4
sp3ncerlan <lansp329@gmail.com>
2026-05-16 12:07:57 -0500

    Go practice

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,29 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"snippetbox.spencerlan.net/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	// Flag setup
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// DB setup
+	dsn := flag.String("dsn", "web:Blue2460!@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	// Logger Setup
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -33,6 +51,19 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- snippetbox/go.mod
+++ snippetbox/go.mod
@@ -5,4 +5,5 @@ go 1.25.0
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	golang.org/x/tour v0.1.0 // indirect
 )
