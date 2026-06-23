b07f824feefbdfe90a8a46dc1a24b2be8dee2b60
sp3ncerlan <lansp329@gmail.com>
2026-06-23 17:19:12 -0500

    update

--- DSA + Interviews/Striver/revision.py
+++ DSA + Interviews/Striver/revision.py
@@ -9,11 +9,10 @@ import bisect
 # k =
 # s = " amazing coding skills "
 # t = "bar"
-arr = [5, 4, 3, 2, 1]
+arr = [3, 2, 1, 4]
 # k = 4
 
 def merge(arr, low, mid, high):
-    inversions = 0
     temp = []
     
     left, right = low, mid + 1
@@ -21,7 +20,6 @@ def merge(arr, low, mid, high):
     while left <= mid and right <= high:
         # compare and merge while counting inverted pairs
         if arr[left] > arr[right]:
-            inversions += (mid - left + 1)
             temp.append(arr[right])
             right += 1
         else:
@@ -38,25 +36,38 @@ def merge(arr, low, mid, high):
 
     for i in range(low, high + 1):
         arr[i] = temp[i - low]
-        
-    return inversions
+
+def countPairs(arr, low, mid, high):
+    pairs = 0
+    
+    left = low
+    for right in range(mid + 1, high + 1):
+        while left <= mid and arr[left] <= 2 * arr[right]:
+            left += 1
+        pairs += (mid - left + 1)
+    
+    return pairs
 
 def mergeSort(arr, low, high):
-    inversions = 0
+    reverse_pairs = 0
     
     if low >= high:
-        return inversions
+        return reverse_pairs
     
     mid = (low + high) // 2
     
-    inversions += mergeSort(arr, low, mid)
-    inversions += mergeSort(arr, mid + 1, high)
-    inversions += merge(arr, low, mid, high)
+    reverse_pairs += mergeSort(arr, low, mid)
+    reverse_pairs += mergeSort(arr, mid + 1, high)
+    reverse_pairs += countPairs(arr, low, mid, high)
     
-    return inversions
+    merge(arr, low, mid, high)
+    
+    return reverse_pairs
 
 print(mergeSort(arr, 0, len(arr) - 1))
 
 """
-BF:
+OPTIMAL:
+- we cannot do the comparison step in the merge step because we are comparing different things
+- we will first compare the values, then merge them
 """
